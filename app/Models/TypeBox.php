cb0d911550e73e95f221c2b93a12721807d74c9c
jorge salgado <jsalgadoecheverria@gmail.com>
2022-03-21 20:57:03 -0500

    first commit

---
+++ app/Models/TypeBox.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TypeBox extends Model
+{
+    use HasFactory;
+    protected $table = 'types_boxes';
+    protected $fillable = ['name'];
+}
